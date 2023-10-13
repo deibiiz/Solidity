e9aea785071ad821329fdba3877c88c302c1854a
deibiiz <61713689+deibiiz@users.noreply.github.com>
2023-10-13 15:57:51 +0200

    Create FundMe.sol

---
+++ FundMe.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+contract FundMe {
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,) = priceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+
+}
