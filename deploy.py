b8d07cfbc925ca6b427758b6c619710cf8a17636
deibiiz <61713689+deibiiz@users.noreply.github.com>
2023-10-16 00:30:57 +0200

    Update deploy.py using brownie

--- deploy.py
+++ deploy.py
@@ -1,97 +1,22 @@
-from solcx import compile_standard, install_solc
-import json
-from web3 import Web3
-import os
-from dotenv import load_dotenv
+from brownie import accounts, config, SimpleStorage, network
 
-load_dotenv()
+def deploy_simple_storage():
+    account = get_account()
+    Simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = Simple_storage.retrieve()
+    print(stored_value)
+    transaction = Simple_storage.store(42, {"from": account})
+    transaction.wait(1) #espere que se complete la transaccion para seguir ejecutando codigo
+    updated_stored_value = Simple_storage.retrieve()
+    print(updated_stored_value)
 
-with open("./SimpleStorage.sol", "r") as file:
-    simple_storage_file = file.read()
-
-
-#compilar
-install_solc("0.6.0")
-compiled_sol = compile_standard(
-    {
-        "language": "Solidity",
-        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
-        "settings": {
-            "outputSelection": {
-                "*":{
-                    "*":["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
-                }
-            }
-        },
-    },
-    solc_version="0.6.0"
-)
-
-with open("compiled_code.json", "w") as file:
-    json.dump(compiled_sol, file)
-
-#bytecode
-#ABI
-
-bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][  
-    "bytecode"
-]["object"]
-
-abi = json.loads(
-    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
-)["output"]["abi"]
-
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-chain_id = 1337
-my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
-private_key = os.getenv("PRIVATE_KEY") #desde la terminal he exportado la private key
-
-# Create the contract in Python
-SimpleStorage = w3.eth.contract(abi = abi, bytecode = bytecode)
-
-# Get the latest transaction
-nonce = w3.eth.getTransactionCount(my_address)
-
-# Submit the transaction that deploys the contract
-transaction = SimpleStorage.constructor().buildTransaction(
-    {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": my_address,
-        "nonce": nonce,
-    }
-)
-# Sign the transaction
-signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"]) #esta forma es rapida solo para cuentas ficticias. -> "brownie accounts new [nombre]".
 
 
-# 3. enviar la transaccion
-tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-print("Esperando a que termine...")
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+def main():
+    deploy_simple_storage()
 
-
-simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-
-#call --> llamado para obtener valor
-#transact --> cuesta gas
-print(simple_storage.functions.retrieve().call())
-
-store_transaction = simple_storage.functions.store(15).buildTransaction(
-    {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": my_address,
-        "nonce": nonce + 1,
-    }
-)
-
-signed_store_txn = w3.eth.account.sign_transaction(
-    store_transaction, private_key=private_key
-)
-print("Updating stored Value...")
-
-t_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(t_hash)
-
-print(simple_storage.functions.retrieve().call())
